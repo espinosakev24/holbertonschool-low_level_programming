5e8c4495d0322d2aae7fa8c959991e793c67a797
Kevin Espinosa <916@holbertonschool.com>
2019-06-19 20:23:50 +0000

    Displaying the size of differents data types using printf and sizeof functions

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main()
+{
+printf("Size of a char: %d bytes(s)\n", sizeof(char));
+printf("Size of a char: %d bytes(s)\n", sizeof(short));
+printf("Size of a char: %d bytes(s)\n", sizeof(int));
+printf("Size of a char: %d bytes(s)\n", sizeof(long));
+printf("Size of a char: %d bytes(s)\n", sizeof(char));
+printf("Size of a char: %d bytes(s)\n", sizeof(long long));
+printf("Size of a char: %d bytes(s)\n", sizeof(float));
+return 0;
+}
