0458a821b4d7fa372574e91fa26c9056c0367d59
Kevin Espinosa <916@holbertonschool.com>
2019-06-20 19:37:47 +0000

    Printing the last digit of a number and declaring two variables

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -18,11 +18,11 @@ int main(void)
 	{
 	printf("Last digit of %d is %d and is greater than 5", n, last);
 	}
-	if (last == 0)
+	else if (last == 0)
 	{
 	printf("Last digit of %d is %d and is 0", n, last);
 	}
-	if (last < 6)
+	else if (last < 6)
 	{
 	printf("Last digit of %d is %d and is less than 6 and not 0", n, last);
 	}
