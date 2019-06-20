d695918de8ab26b38e3bdb178d91c62f806dc9b3
Kevin Espinosa <916@holbertonschool.com>
2019-06-20 20:30:53 +0000

    using putchar and a while loop for printing the alphabet in uppercase

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int n;
+n = 65;
+while (n <= 90)
+{
+putchar(n);
+n++;
+}
+putchar('\n');
+return (0);
+}#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int n;
+n = 97;
+while (n <= 122)
+{
+putchar(n);
+n++;
+}
+putchar('\n');
+return (0);
+}
