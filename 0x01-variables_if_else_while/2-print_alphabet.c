fe99e5cbc941a387ae8af4817693d15e5d3099b6
Kevin Espinosa <916@holbertonschool.com>
2019-06-20 20:23:41 +0000

    using putchar and a while loop for printing the alphabet in lower case

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int n;
+n = 97;
+while (n <= 122)
+{
+putchar(n);
+putchar('\n');
+n++;
+}
+return (0);
+}
+
