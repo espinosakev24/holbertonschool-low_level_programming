b5a215a8b7d87a7daee9abf481f7165be6a0a474
Kevin Espinosa <916@holbertonschool.com>
2019-06-20 17:04:20 +0000

    if statment for generate a random number that can be both positive and negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,5 +1,5 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
 /**
  * main - Entry point
@@ -14,15 +14,15 @@ n = rand() - RAND_MAX / 2;
 /* your code goes there */
 if (n > 0)
 {
-printf("%d", n);
+printf("%d is positive\n", n);
 }
 if (n < 0)
 {
-printf("%d", n);
+printf("%d is negative\n", n);
 }
 if (n == 0)
 {
-printf("%d", n);
+printf("%d is zero\n", n);
 }
 return (0);
 }
