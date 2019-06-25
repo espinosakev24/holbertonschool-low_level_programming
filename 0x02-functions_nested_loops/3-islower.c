216762d356aa464a9ee3db0fb8b5fed6242af792
Kevin Espinosa <916@holbertonschool.com>
2019-06-25 18:36:58 +0000

    checking if a character is uppercase or lowercase

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,8 +1,13 @@
 #include "holberton.h"
+/**
+* _islower -  check the code for Holberton School students.
+* @c: lower case
+* Return: Always 1 or 0
+*/
 int _islower(int c)
 {
 
-	if((c <= 122) && (c >= 97))
+	if ((c <= 122) && (c >= 97))
 	{
 		return (1);
 	}
