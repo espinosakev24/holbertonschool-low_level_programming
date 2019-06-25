d2b816fe6a9b2c37124f6e2af34a9944193db257
Kevin Espinosa <916@holbertonschool.com>
2019-06-25 18:57:04 +0000

    checking with functions if a letter is in upper or lowercase

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ * _isalpha - check the code for Holberton School students.
+ * @c: upper or lowercase
+ * Return: Always lower or uppercase letter.
+ */
+int _isalpha(int c)
+{
+	if (((c >= 65) && (c <= 90)) || ((c >= 97) && (c <= 122)))
+	{
+	return (1);
+	return (0);
+	}
+}
