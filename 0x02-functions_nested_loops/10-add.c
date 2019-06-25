5eab0dfbe189ec704ea5b9a774f2f703d75126bd
Kevin Espinosa <916@holbertonschool.com>
2019-06-25 22:50:39 +0000

    uploading files

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,4 +1,10 @@
 #include "holberton.h"
+/**
+ * add - check the code for Holberton School students.
+ * @a: an intenger
+ * @b: an intenger
+ * Return: Always adition between two numbers.
+ */
 int add(int a, int b)
 {
 	return (a + b);
