8fed2261fc26d783e18997c7f35e922bf20efd0a
Kevin Espinosa <916@holbertonschool.com>
2019-06-25 15:07:39 +0000

    printing alphabet using function defined in holberton.h

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,16 @@
 #include "holberton.h"
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
 void print_alphabet(void)
 {
 int n;
 
 n = 97;
 
-while(n < 123)
+while (n < 123)
 {
 	_putchar(n);
 	n++;
