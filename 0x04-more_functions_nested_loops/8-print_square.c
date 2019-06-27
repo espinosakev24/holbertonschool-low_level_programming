684f0bc0754da5873ebcfe08848e0e3f27f4f306
Kevin Espinosa <916@holbertonschool.com>
2019-06-27 22:25:14 +0000

    printing a square using nested loop functions

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+/**
+ * print_square - check the code for Holberton School students.
+ * @size: variable that give values to the function
+ * Return: void.
+ */
+void print_square(int size)
+{
+int a, b;
+
+if (size == 0)
+{
+	_putchar('\n');
+}
+else
+{
+	for (a = 1; a <= size; a++)
+	{
+		for (b = 1; b <= size; b++)
+		{
+			_putchar(35);
+		}
+		_putchar('\n');
+}
+}
+}
