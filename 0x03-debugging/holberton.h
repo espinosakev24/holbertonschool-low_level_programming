60d98bd48200af1141426213f434ff8799f03ed7
Kevin Espinosa <916@holbertonschool.com>
2019-06-27 04:29:49 +0000

    uploading holberton.h with include lines

--- 0x03-debugging/holberton.h
+++ 0x03-debugging/holberton.h
@@ -1,17 +1,11 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
 #include "holberton.h"
-
-/**
- * largest_number - returns the largest of 3 numbers
- * @a: first integer
- * @b: second integer
- * @c: third integer
- * Return: largest number
- */#include "holberton.h"
-
-/**
+/*
  * largest_number - returns the largest of 3 numbers
  * @a: first integer
  * @b: second integer
  * @c: third integer
  * Return: largest number
  */
+#endif
