5e3b4c42c33934eeee9fa9581c41f9cfd3b1013c
Kevin Espinosa <916@holbertonschool.com>
2019-07-02 15:47:39 +0000

    using putchar to puts numbers with specific conditions

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,4 +1,9 @@
 #include "holberton.h"
+/**
+ * puts2 - check the code for Holberton School students.
+ * @str: variable in which numbers are stored
+ * Return: Always 0.
+ */
 
 void puts2(char *str)
 {
