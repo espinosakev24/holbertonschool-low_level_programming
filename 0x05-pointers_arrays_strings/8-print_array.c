3f4b5445855db3646927d38bab20c53f79e6b896
Kevin Espinosa <916@holbertonschool.com>
2019-07-02 21:16:31 +0000

    printing content of each position in an array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,21 +1,27 @@
 #include "holberton.h"
 #include <stdio.h>
+/**
+ * print_array - check the code for Holberton School students.
+ * @a: pointer to array
+ * @n: intenger that tells the position in an array
+ * Return: Always void.
+ */
 void print_array(int *a, int n)
 {
-	int b = 0;
+int b = 0;
 
-	while (b < n)
+while (b < n)
+{
+	if (b < n - 1)
 	{
-		if (b < n - 1)
-		{
-				printf("%d, ", a[b]);
-		}
-		else
-		{
-			printf("%d", a[b]);
-			break;
-		}
-		b++;
+		printf("%d, ", a[b]);
+	}
+	else
+	{
+		printf("%d", a[b]);
+		break;
+	}
+	b++;
 	}
 printf("\n");
 }
