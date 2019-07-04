726b875192010e7d3574d0b22226db963155a736
Kevin Espinosa <916@holbertonschool.com>
2019-07-04 17:08:04 +0000

    Learning how to concatenating strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,7 +1,14 @@
 #include "holberton.h"
+/**
+ * _strcat - check the code for Holberton School students.
+ * @dest: hello
+ * @src: world!
+ * Return: Always 0.
+ */
 char *_strcat(char *dest, char *src)
 {
 	int len, n;
+
 	n = 0;
 	len = 0;
 
