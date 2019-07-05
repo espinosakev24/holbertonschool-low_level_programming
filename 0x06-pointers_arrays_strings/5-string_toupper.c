47f8a788d68fab4dc9a3bad9f5be32ff88910eb2
Kevin Espinosa <916@holbertonschool.com>
2019-07-05 00:22:51 +0000

    converting to uppercase

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,16 +1,21 @@
 #include "holberton.h"
+/**
+ * string_toupper - check the code for Holberton School students.
+ * @s: pointer
+ * Return: Always 0.
+ */
 char *string_toupper(char *s)
 {
 	int a;
 
 	while (s[a] != 0)
 	{
-		if ( s[a] >= 'a' && 'z')
+		if (s[a] >= 'a' && 'z')
 		{
 			s[a] = s[a] - 32;
 
 		}
 		a++;
 	}
-return(s);
+return (s);
 }
