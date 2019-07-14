b1ea24cd01b684d4b8e36519da80c0a31fa3e3b0
Kevin Espinosa <916@holbertonschool.com>
2019-07-14 16:15:08 -0500

    printing the amount of parammeters

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(int argc, __attribute__((unused)) char *argv[])
+{
+	argc = argc - 1;
+	printf("%d", argc);
+	return (0);
+}
