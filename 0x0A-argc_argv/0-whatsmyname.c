5d0f39d6a11308acf438a98777e0220bf3b03957
Kevin Espinosa <916@holbertonschool.com>
2019-07-14 15:44:08 -0500

    Program that prints its name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
-
+/**
+ * main - check the code for Holberton School students.
+ * @argc: counter of number of parameters of a command
+ * @argv: array of pointers
+ * Return: Always 0.
+ */
 int main(int argc, char *argv[])
 {
-int n;
 
-n = 0;
-
-	if (argc != n)
+	if (argc != 0)
 	{
 		printf("%s\n", argv[0]);
 	}
