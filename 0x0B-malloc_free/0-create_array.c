774abb36f9503963b1b8ac7a5d93aa2a0ac8f442
Kevin Espinosa <916@holbertonschool.com>
2019-07-16 14:03:24 -0500

    function that creates an array filled with chars

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * create_array - function that creates an array filled with a character
+ * @size: the size of the memory to print
+ * @c: char that will fill the space reserved
+ * Return: pointer to the first adress reserved.
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	unsigned int n = 0;
+	char *s;
+
+	s = malloc(sizeof(char) * size);
+
+
+		if (size == 0)
+		{
+			return (NULL);
+		}
+
+	while (n < size)
+	{
+		*(s + n) = c;
+		n++;
+	}
+	return (s);
+}
