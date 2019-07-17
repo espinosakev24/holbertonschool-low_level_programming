258a09a20bc2f0d66c86e74fba9fcd96923fd22b
Kevin Espinosa <916@holbertonschool.com>
2019-07-17 23:21:54 +0000

    function that return arguments listed

--- 0x0B-malloc_free/5-argstostr.c
+++ 0x0B-malloc_free/5-argstostr.c
@@ -8,9 +8,35 @@
  */
 char *argstostr(int ac, char **av)
 {
-	char *s = NULL;
+	int a, b, c = 0;
+	char *s, *p;
 
-	int, a = 0, b = 0, c = 0, d = 0;
+	if (ac == 0 || av == 0)
+		return (NULL);
 
-	return (s);
+	for (a = 0; a < ac; a++)
+	{
+		for (b = 0; av[a][b] != 0; b++)
+		{
+			c++;
+		}
+	}
+	s = malloc(sizeof(char) * c + ac + 1);
+	p = s;
+	if (s == 0)
+	{
+		return (NULL);
+	}
+
+	for (a = 0; a < ac; a++)
+	{
+		for (b = 0; av[a][b] != 0; b++)
+		{
+			*s = av[a][b];
+			s++;
+		}
+	*s = '\n';
+	s++;
+	}
+	return (p);
 }
