27d3496940b63757de4f569c8686497af15d6716
Kevin Espinosa <916@holbertonschool.com>
2019-07-16 22:43:50 -0500

    filling requirements

---
+++ 0x0B-malloc_free/100-strtow.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * strtow - prints a grid of integers
+ * @str: pointer
+ *
+ * Return: Nothing.
+ */
+char **strtow(char *str)
+{
+	if (*str == NULL || *str == "")
+	{
+		return (NULL);
+	}
+}
---
+++ 0x0B-malloc_free/5-argstostr.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * argstostr - prints a grid of integers
+ * @ac: the address of the two dimensional grid
+ * @av: height of the grid
+ *
+ * Return: Nothing.
+ */
+char *argstostr(int ac, char **av)
+{
+	int *s;
+
+	if (ac == 0 || av == NULL)
+	{
+		return (NULL);
+	}
+	if (s == NULL)
+	{
+		return (NULL);
+	}
+}
