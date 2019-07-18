035cd9354d4e843fb3196d39b7255a8237d252cd
Kevin Espinosa <916@holbertonschool.com>
2019-07-18 16:19:53 -0500

    creating calloc function using malloc and filling the pointer s with 0

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,12 +1,8 @@
-#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include "holberton.h"
 /**
  * _calloc - prints buffer in hexa
  * @nmemb: the address of memory to print
  * @size: the size of the memory to print
- *
  * Return: s pointer with the space filled with 0
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
@@ -14,17 +10,19 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	unsigned int n;
 	unsigned int *s;
 
-	if (nmemb == 0)
+	if (nmemb == 0 || size == 0)
 	{
 		return (NULL);
 	}
-	s = malloc(nmemb * (size));
-	if (s == 0)
-		return (0);
 
-	for (n = 0; s[n] != 0; n++)
+	s = malloc(nmemb * (size));
+
+	if (s == NULL)
+		return (NULL);
+
+	for (n = 0; s[n] != '\0'; n++)
 	{
-		s[n] = 0;
+		s[n] = '\0';
 	}
 	return (s);
 }
