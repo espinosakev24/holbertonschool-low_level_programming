6ebc1a5c9b0d1f5e3d0239b0e39fa545e36769b1
Kevin Espinosa <916@holbertonschool.com>
2019-07-22 12:21:53 -0500

    learning about structures in c

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,16 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+/**
+ * struct dog - structure type containing django datas.
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: dog's owner
+ * Return: Always 0.
+ */
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 }
+#endif
