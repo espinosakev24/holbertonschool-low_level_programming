c5da8dc5a5b8d429024f8ce93c69823db8eb0f73
Kevin Espinosa <916@holbertonschool.com>
2019-07-22 13:07:37 -0500

    dog header with all the prototypes of the functions

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -12,7 +12,7 @@ struct dog
 	char *name;
 	float age;
 	char *owner;
-}
+} dog;
 int _putchar(char c);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
