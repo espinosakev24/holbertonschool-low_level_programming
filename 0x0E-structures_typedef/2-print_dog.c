28131cf23da760bc3e995e0b0b04b6f749d71490
Kevin Espinosa <916@holbertonschool.com>
2019-07-22 15:52:28 -0500

    function that prints the varialbes and its value of dog struct

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -20,17 +20,11 @@ void print_dog(struct dog *d)
 		}
 		else
 		{
-		printf("Name: %s\n", d->name);
+			printf("Name: %s\n", d->name);
 		}
 
-		if (d->name == NULL)
-		{
-			printf("Age: (nil)\n");
-		}
-		else
-		{
-			printf("Age: %f\n", d->age);
-		}
+		printf("Age: %f\n", d->age);
+
 		if (d->owner == NULL)
 		{
 			printf("Owner: (nil)");
