3f2ddf194a33d163814c388d6db3b89f576140a9
Kevin Espinosa <916@holbertonschool.com>
2019-07-27 20:12:44 +0000

    function that prints all

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+/**
+ * print_all - prints whatever thing passed through its parameter.
+ * @format: format of the parameters
+ */
+void print_all(const char * const format, ...)
+{
+	va_list a;
+	char *s;
+	int n = 0;
+
+	va_start(a, format);
+	while (format == 0)
+	{	printf("\n");
+		return;
+	}
+	while (format[n] != 0)
+	{
+		switch (format[n])
+		{
+			case 'c':
+				printf("%c", (char)va_arg(a, int));
+			break;
+			case 'i':
+				printf("%d", va_arg(a, int));
+			break;
+			case 'f':
+				printf("%f", (float)va_arg(a, double));
+			break;
+			case 's':
+				s = va_arg(a, char *);
+				if (s != NULL)
+				{
+					printf("%s", s);
+					break;
+				}
+					printf("(nill)");
+			break;
+		}
+		if ((format[n] == 'c' || format[n] == 'i' || format[n] == 'f' ||
+		format[n] == 's') && (format[n + 1] != 0))
+		{	printf(", ");
+		}
+	n++;
+	}
+	va_end(a);
+	putchar('\n');
+}
+#include "variadic_functions.h"
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	print_all("ceis", 'H', 0, "lberton");
+	return (0);
+}
