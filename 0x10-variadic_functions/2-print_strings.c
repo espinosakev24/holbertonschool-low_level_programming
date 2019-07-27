3e56bf3c58bddeb97fad7446dc9aee25d2c272fb
Kevin Espinosa <916@holbertonschool.com>
2019-07-27 17:06:29 +0000

    function that prints strings

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -40,13 +40,3 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	va_end(valist);
 	putchar('\n');
 }
-/**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
- */
-int main(void)
-{
-print_strings(", ", 2, "Jay", "Django");
-return (0);
-}
