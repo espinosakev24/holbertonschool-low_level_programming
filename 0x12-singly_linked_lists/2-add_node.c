f07e380b83b48e68b010653eb22909821d73b907
Kevin <916@holbertonschool.com>
2019-08-01 22:43:27 +0000

    function that adds a node to the begging of the list

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include "lists.h"
+#include <stdlib.h>
+#include <string.h>
+/**
+ * add_node - function adds node at the begging of a list.
+ * @head: Double point to head of the list
+ * @str: pointer to the string of the list
+ * Return: kevnode.
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *kevnode = malloc(sizeof(list_t));
+
+	if (kevnode == NULL)
+	{
+		return (NULL);
+	}
+	kevnode->str = strdup(str);
+	kevnode->len = _strlen(kevnode->str);
+	kevnode->next = *head;
+	*head = kevnode;
+
+	return (kevnode);
+}
+/**
+ * _strlen - check the code for Holberton School students.
+ * @s: pointer to holberton
+ * Return: Always n.
+ */
+int _strlen(char *s)
+{
+	int n;
+
+	n = 0;
+
+	while (*s != '\0')
+	{
+		n++;
+		s++;
+	}
+	return (n);
+}
+
