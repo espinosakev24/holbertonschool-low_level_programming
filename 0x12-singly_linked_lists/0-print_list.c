a8d18b22084adcc726f691d12d8e4f8793dfafca
Kevin <916@holbertonschool.com>
2019-08-01 19:54:35 +0000

    function that prints the amount of nodes

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,35 +1,30 @@
 #include <stdio.h>
 #include "lists.h"
 /**
- *
- *
- *
+ * print_list - function that prints the amount of nodes.
+ * @h: The pointer that points to header of the struct
+ * Return: n.
  */
 size_t print_list(const list_t *h)
 {
-	char * str;
 	int n = 0;
-	str = h->str;
+
 	if (h == NULL)
 	{
 		return (0);
 	}
-	else
+	while (h != NULL)
 	{
-		while (h != NULL)
+		if (h->str == NULL)
 		{
-			if (h->str == NULL)
-			{
-				printf("[0] (nil)\n");
-			}
-
-			else
-			{
-				printf("[%d] %s\n", h->len, h->str);
-			}
-			n++;
-			h = h->next;
+			printf("[0] (nil)\n");
 		}
-		return(n);
+		else
+		{
+			printf("[%d] %s\n", h->len, h->str);
+		}
+		n++;
+		h = h->next;
 	}
+	return (n);
 }
