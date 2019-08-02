4e4d23bcecf8097a550b925b6168e6deab1212f8
Kevin <916@holbertonschool.com>
2019-08-01 22:23:23 -0500

    header of the functions

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -22,4 +22,5 @@ int _strlen(const char *s);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
+void myStartupFun(void);
 #endif
