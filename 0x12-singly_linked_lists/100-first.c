457ab38a9be0fbe240f32359096ccaee9bfe6618
Kevin <916@holbertonschool.com>
2019-08-01 22:35:59 -0500

    function that is executed before the main

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -4,7 +4,7 @@
  * myStartupFun - function that prints before main
  * Return: void.
  */
-void myStartupFun(void)
+void mystartupsun(void)
 {
 	printf("You're beat! and yet, you must
 	allow,\nI bore my house upon my back!\n");
