bb5691427d65414535f2d434af945615b8b8b2b3
Kevin <916@holbertonschool.com>
2019-08-01 21:16:33 -0500

    header with the prototypes of the functions

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -20,4 +20,5 @@ size_t list_len(const list_t *h);
 void free_list(list_t *head);
 int _strlen(const char *s);
 list_t *add_node(list_t **head, const char *str);
+void free_list(list_t *head);
 #endif
