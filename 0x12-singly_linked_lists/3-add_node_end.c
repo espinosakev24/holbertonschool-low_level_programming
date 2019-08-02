660bc502c4aab0736c1bccaee7f17d3080a6d79d
Kevin <916@holbertonschool.com>
2019-08-02 00:16:39 +0000

    function that addas a node to the end of the list

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "lists.h"
+#include <stdlib.h>
+#include <string.h>
+/**
+ * add_node - function adds node at the begging of a list.
+ * @head: Double point to head of the list
+ * @str: pointer to the string of the list
+ * Return: kevnode.
+ */
+list_t *add_node_end(list_t **head, const char *str);
+{
+	list_t *kevnode = malloc(sizeof(list_t));
+
+	if (kevnode == NULL)
+	{
+		return (NULL);
+	}
+	while (*head != NULL)
+	{
+		header = header->next;
+	}
+}
