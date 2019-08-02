47fe202a76ce854f0b5bd83a65157eb11332b08b
Kevin <916@holbertonschool.com>
2019-08-01 22:42:45 -0500

    function before main

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -22,5 +22,5 @@ int _strlen(const char *s);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
-void startupfun(void) __atribute__((constructor));
+void startupfun (void) __atribute__ ((constructor));
 #endif
