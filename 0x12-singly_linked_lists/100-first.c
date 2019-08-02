513f03afdd5474074989fdf79ea8abb50c097eb1
Kevin <916@holbertonschool.com>
2019-08-01 23:05:11 -0500

    function that prints before main

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include "lists.h"
-void startupfun (void) __attribute__ ((constructor));
+void startupfun(void) __attribute__((constructor));
 /**
  * startupfun - Function to execueted before the main.
  *
@@ -9,6 +9,7 @@ void startupfun (void) __attribute__ ((constructor));
 void startupfun(void)
 {
 	char *str;
+
 	str = "I bore my house upon my back!";
 	printf("You're beat! and yet, you must allow,\n%s\n", str);
 }
