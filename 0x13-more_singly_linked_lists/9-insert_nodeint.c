485da19ab97b0b9d4ea3725ace1d14bee35298af
Kevin <916@holbertonschool.com>
2019-08-07 03:46:48 +0000

    function that inserts a node in a nth position

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -9,23 +9,30 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	unsigned int a = 0;
 	listint_t *new_node = malloc(sizeof(listint_t));
+	listint_t *tmp;
 
 	if (new_node == NULL)
 		return (NULL);
 
 	new_node->n = n;
 
-	while (*head != NULL)
+	if (idx == 0)
+	{
+		new_node->next = *head;
+		*head = new_node;
+		return (new_node);
+	}
+	tmp = *head;
+	while (tmp != NULL)
 	{
 		if (a == idx - 1)
 		{
-			new_node->next = (*head)->next;
-			(*head)->next = new_node;
+			new_node->next = tmp->next;
+			tmp->next = new_node;
 			return (new_node);
 		}
 		a++;
-		*head = (*head)->next;
+		tmp = tmp->next;
 	}
-	new_node = *head;
 	return (new_node);
 }
