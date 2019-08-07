fda04fdb2012d7a7d86ee9ae426945d02b99861b
Kevin <916@holbertonschool.com>
2019-08-07 17:25:34 +0000

    function that delete a node in a nth position

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include "lists.h"
+/**
+ *
+ *
+ *
+ *
+ */
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	unsigned int n = 0;
+	listint_t *tmp = *head, *aux;
+
+	if (*head == NULL)
+		return (0);
+
+	if (head == NULL)
+		return (0);
+
+	while (tmp != NULL)
+	{
+		if (n == index - 1)
+		{
+			aux = tmp->next;
+			free(tmp->next);
+			tmp->next = aux;
+			return (index);
+		}
+		n++;
+		tmp = tmp->next;
+	}
+		return (-1);
+}
