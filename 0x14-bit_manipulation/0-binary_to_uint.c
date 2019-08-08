cd1b85e4ade3718fe2e0380b7d304596214259c3
Kevin <916@holbertonschool.com>
2019-08-08 23:02:58 +0000

    function that converst bin to dec

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+ * binary_to_uint - function that converts a binary into a dec
+ * @b: pointer to the string of binary numbers
+ * Return: result
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int p2[32], a = 0, pot = 1, n = 0, result = 0;
+	unsigned int n2 = 0;
+
+	while (a < 32)
+	{
+		if (a == 0)
+		{
+			p2[a] = 1;
+			a++;
+			continue;
+		}
+			pot = pot * 2;
+			p2[a] = pot;
+			a++;
+	}
+	for (n = 0; b[n] != '\0'; n++)
+	{}
+
+	while (n > 0)
+	{
+		if (b[n - 1] != '0' && b[n - 1] != '1')
+			return (0);
+
+		if (b[n - 1] == '1')
+		{
+			result = result + p2[n2];
+		}
+		n--;
+		n2++;
+	}
+
+	return (result);
+}
