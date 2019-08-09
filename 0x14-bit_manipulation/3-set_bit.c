af761c324952ced8fc6487f59600f4563cb3fbd9
Kevin <916@holbertonschool.com>
2019-08-09 03:48:23 +0000

    function that sets a bit

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+ * set_bit - function that sets a bit to 1
+ * @index: index of the function
+ * @n: pointer to the number
+ * Return: 1
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > 64)
+		return (-1);
+}
