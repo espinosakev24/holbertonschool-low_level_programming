f3d342a25e6f040e1e0fdb0121d0621d980cdc6a
Kevin <916@holbertonschool.com>
2019-08-09 03:51:01 +0000

    function that sets a bit

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -4,10 +4,14 @@
  * set_bit - function that sets a bit to 1
  * @index: index of the function
  * @n: pointer to the number
- * Return: 1
+ * Return: 0;
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
 	if (index > 64)
 		return (-1);
+
+	*n = 1;
+	index = 1;
+	return (0);
 }
