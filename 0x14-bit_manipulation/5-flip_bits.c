c56fd0cca3cf2382e65f930a3d2f1e1bf1733110
Kevin <916@holbertonschool.com>
2019-08-09 03:58:31 +0000

    pending task for solve dudes in pld

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+ * flip_bits - fun flip bit
+ * @m: number2
+ * @n: number
+ * Return: 0;
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	if (m > 64)
+		return (-1);
+
+	n = 1;
+	m = 1;
+	return (0);
+}
