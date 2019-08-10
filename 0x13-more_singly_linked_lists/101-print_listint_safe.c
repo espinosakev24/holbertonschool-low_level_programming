1a7b8eb9c808a19ca5da581188241e6ff45acab3
Kevin <916@holbertonschool.com>
2019-08-10 02:28:00 +0000

    header file

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -3,7 +3,6 @@
 /**
  * print_listint_safe - function that deletes a node in a nth position
  * @head: head pointer of the function
- * @index: iterator
  * Return: the index
  */
 size_t print_listint_safe(const listint_t *head)
