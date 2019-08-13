47e02f1e1bb56fb2fba5c35c53509cdf6179734c
Kevin <916@holbertonschool.com>
2019-08-13 22:23:24 +0000

    function that creates a file and write on it

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -4,31 +4,28 @@
 #include <fcntl.h>
 #include "holberton.h"
 /**
- *
- *
- *
- *
+ * create_file - function that creates a file
+ * @filename: name of the file
+ * @text_content: content of the file
+ * Return: 1
  */
 int create_file(const char *filename, char *text_content)
 {
 	int fd = open(filename, O_CREAT | O_TRUNC | O_RDWR, 0600);
 	int i = 0, a = 0;
 
+	if (text_content == NULL)
+	{	close(fd);
+		return (1);
+	}
 	i = _strlen(text_content);
 	if (filename == NULL)
-	{
 		return (-1);
-	}
 	if (fd == -1)
 		return (-1);
 
 	if (fd == -1)
-	{
-		return (0);
-	}
-	if (text_content == NULL)
-	{
-		open(filename, O_CREAT, 0600);
+	{	return (0);
 	}
 	a = write(fd, text_content, i);
 	if (a == -1)
