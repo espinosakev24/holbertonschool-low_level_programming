6369a0a0dc1181302763714ea85a8d5afa0851e8
Kevin <916@holbertonschool.com>
2019-08-14 00:54:33 +0000

    function that appends text

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -10,8 +10,8 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	ssize_t fd = open(filename, O_APPEND | O_WRONLY, 0600);
-	int a = 0, i = 0;
+	ssize_t fd = open(filename, O_APPEND | O_WRONLY, 0600), a = 0;
+	int i = 0;
 
 	if (text_content == NULL)
 		return (1);
@@ -25,6 +25,8 @@ int append_text_to_file(const char *filename, char *text_content)
 	a = write(fd, text_content, i);
 
 	if (a == -1)
+		return (-1);
+	if (a != i)
 		return (-1);
 	close(fd);
 	return (1);
