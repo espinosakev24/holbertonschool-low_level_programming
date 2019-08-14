132fcfe043ab070cf3a98da48407b9ebd517e3f5
Kevin <916@holbertonschool.com>
2019-08-14 00:41:19 +0000

    function that appends text

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include "holberton.h"
+/**
+ * append_text_to_file - func that append a text to the end of a file
+ * @filename: name of the file
+ * @text_content: content of the file
+ * Return: 1
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	ssize_t fd = open(filename, O_APPEND | O_WRONLY, 0600);
+	int a = 0, i = 0;
+
+	if (text_content == NULL)
+		return (1);
+	i = _strlen(text_content);
+	if (filename == NULL)
+		return (-1);
+
+	if (fd == -1)
+		return (-1);
+
+	a = write(fd, text_content, i);
+
+	if (a == -1)
+		return (-1);
+	close(fd);
+	return (1);
+}
+/**
+ * _strlen - check the code for Holberton School students.
+ * @s: pointer to holberton
+ * Return: Always n.
+ */
+int _strlen(char *s)
+{
+	int n;
+
+	n = 0;
+
+	while (*s != '\0')
+	{
+		n++;
+		s++;
+	}
+	return (n);
+}
