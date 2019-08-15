723b84d7bca81fff3b7a82bb62bc5d758ff0f76e
Kevin <916@holbertonschool.com>
2019-08-15 19:41:04 +0000

    function that cp content of a file into other file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -4,10 +4,60 @@
 #include <fcntl.h>
 #include "holberton.h"
 /**
- * main - function that copies file to file content
- * Return: 1
+ * cp_file_content - function that copies content of a file src to dest file
+ * @src: file from content will be copied
+ * @dest: file where the content will be pasted
+ * Return: 0
  */
-int main(void)
+int cp_file_content(const char *src, char *dest)
 {
-	return (1);
+	ssize_t fd_src = open(src, O_RDONLY);
+	ssize_t fd_dest = open(dest, O_WRONLY | TRUNC);
+	int read_src = 0;
+	if (fd_src == -1)
+	{
+		char *err_1 = "Error: Can't read from file NAME_OF_THE_FILE\n";
+		int size_1 = _strlen(err_1);
+
+		write(STDERR_FILENO, err_1, size_1);
+		exit(98);
+	}
+	return (0);
+}
+/**
+ * main - man function
+ * @argc: amount of parameters
+ * @argv: pointer to each parameter
+ * Return: 0.
+ */
+int main(int argc, char *argv[])
+{
+	char *serr1 = "Usage: cp file_from file_to\n";
+	int size = _strlen(serr1);
+
+	if (argc != 3)
+	{
+		write(STDERR_FILENO, serr1, size);
+		exit(97);
+	}
+	cp_file_content(argv[1], argv[2]);
+	return (0);
+}
+/**
+ * _strlen - check the code for Holberton School students.
+ * @s: pointer to holberton
+ * Return: Always n.
+ */
+int _strlen(char *s)
+{
+	int n;
+
+	n = 0;
+
+	while (*s != '\0')
+	{
+		n++;
+		s++;
+	}
+	return (n);
 }
