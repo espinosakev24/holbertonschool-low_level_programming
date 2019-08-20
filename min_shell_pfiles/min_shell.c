4d667ae3cf81e4b97af785228a51ddc081f8048c
Kevin <916@holbertonschool.com>
2019-08-20 02:35:16 +0000

    adding wait and fork function

--- min_shell_pfiles/min_shell.c
+++ min_shell_pfiles/min_shell.c
@@ -5,25 +5,40 @@
 
 int main(void)
 {
-	int buff_size = 0;
-	char *buffer;
+	int buff_size = 0, pid, child_p;
+	char *buffer, *argv[2];
 	size_t size = 32;
 
 	buffer = malloc((sizeof(char)) * size);
 	if (buffer == NULL)
 		exit(1);
-
-	char *argv[2];
-
 	printf("$ ");
+	argv[0] = buffer, argv[1] = NULL;
 	while ((getline(&buffer, &size, stdin)) != EOF)
 	{
-		buff_size = strlen(buffer);
-		buffer[buff_size - 1] = '\0';
-		printf("$ ");
-		argv[0] = buffer, argv[1] = NULL;
-		if (execve(argv[0], argv, NULL) == -1)
+		pid = fork();
+		child_p = pid;
+		if (pid == -1)
+		{
 			perror("Error");
+			exit(1);
+		}
+		if (pid == 0)
+		{
+			buff_size = strlen(buffer);
+			buffer[buff_size - 1] = '\0';
+		
+			if (execve(argv[0], argv, NULL) == -1)
+			{
+				perror("Error");
+				exit(EXIT_FAILURE);
+			}
+		}
+		else
+			wait(&child_p);
+
+		printf("$ ");
+
 	}
 	return(0);
 }
