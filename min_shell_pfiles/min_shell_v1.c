86937964420f80ee674316a472a6f531562ff7a8
Kevin <916@holbertonschool.com>
2019-08-20 20:23:52 +0000

    adding the strtok function to separate by tokens the input

--- min_shell_pfiles/min_shell_v1.c
+++ min_shell_pfiles/min_shell_v1.c
@@ -9,32 +9,37 @@
  */
 int main(void)
 {
-	int buff_size = 0, pid, child_p;
+	char *tokens[1024];
+	int buff_size = 0, pid, child_p, n = 0;
 	char *buffer, *argv[2];
 	size_t size = 32;
 
 	buffer = malloc((sizeof(char)) * size);
 	if (buffer == NULL)
 		exit(1);
+	/* shell with no arguments*/
 	printf("$ ");
 	argv[0] = buffer, argv[1] = NULL;
 	while ((getline(&buffer, &size, stdin)) != EOF)
 	{
+		tokens[0] = strtok(buffer, " ");
+		while (tokens[n] != NULL)
+		{
+			n++;
+			tokens[n] = strtok(NULL, " ");
+		}
 		pid = fork();
 		child_p = pid;
 		if (pid == -1)
-		{
-			perror("Error");
+		{	perror("Error");
 			free(buffer);
 			exit(1);
 		}
 		if (pid == 0)
-		{
-			buff_size = strlen(buffer);
+		{	buff_size = strlen(buffer);
 			buffer[buff_size - 1] = '\0';
 			if (execve(argv[0], argv, NULL) == -1)
-			{
-				perror("Error:");
+			{	perror("Error:");
 				free(buffer);
 				exit(EXIT_FAILURE);
 			}
@@ -44,5 +49,6 @@ int main(void)
 
 		printf("$ ");
 	}
+	/* shell with no arguments*/
 	return (0);
 }
