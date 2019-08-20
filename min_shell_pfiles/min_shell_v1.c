ca9f110bc7141478dce028685cf32d1adbc91904
Kevin <916@holbertonschool.com>
2019-08-20 23:45:51 +0000

    shell_v1: correcting static variable

--- min_shell_pfiles/min_shell_v1.c
+++ min_shell_pfiles/min_shell_v1.c
@@ -9,8 +9,9 @@
  */
 int main(void)
 {
+	static int main_var = 1;
 	char *tokens[1024];
-	int buff_size = 0, pid, child_p, n = 0;
+	int buff_size = 0, pid, child_p, n = 1;
 	char *buffer, *argv[2];
 	size_t size = 32;
 
@@ -18,36 +19,39 @@ int main(void)
 	if (buffer == NULL)
 		exit(1);
 	/* shell with no arguments*/
-	printf("$ ");
 	argv[0] = buffer, argv[1] = NULL;
-	while ((getline(&buffer, &size, stdin)) != EOF)
+	while (main_var)
 	{
-		tokens[0] = strtok(buffer, " ");
-		while (tokens[n] != NULL)
+		printf("$ ");
+		if ((getline(&buffer, &size, stdin)) == EOF)
 		{
-			n++;
-			tokens[n] = strtok(NULL, " ");
+			main_var = 0;
+			continue;
 		}
 		pid = fork();
-		child_p = pid;
 		if (pid == -1)
 		{	perror("Error");
 			free(buffer);
 			exit(1);
 		}
-		if (pid == 0)
-		{	buff_size = strlen(buffer);
+		else if (pid == 0)
+		{
+			tokens[0] = strtok(buffer, " ");
+			while ((tokens[n] = strtok(NULL, " ")))
+			{
+				n++;
+			}
+			printf("%s\n", tokens[2]);
+			buff_size = strlen(buffer);
 			buffer[buff_size - 1] = '\0';
 			if (execve(argv[0], argv, NULL) == -1)
-			{	perror("Error:");
+			{	perror("Error");
 				free(buffer);
 				exit(EXIT_FAILURE);
 			}
 		}
 		else
 			wait(&child_p);
-
-		printf("$ ");
 	}
 	/* shell with no arguments*/
 	return (0);
