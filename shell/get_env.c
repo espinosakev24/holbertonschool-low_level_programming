2ca8ff92268f798c72d381331e07102b6965db2b
Kevin <916@holbertonschool.com>
2019-08-22 23:44:30 +0000

    function that gets the env

---
+++ shell/get_env.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+extern char **environ;
+
+char *path(char *PATH) 
+{
+	char **ev;
+	char *var;
+
+	for (ev = environ; *ev != NULL; ev++)
+	{
+		if (strcmp(strtok(*ev, "="), PATH) == 0)
+		{
+			var = strtok(NULL, "=");
+			return (var);
+		}
+	}
+	return (NULL);
+}
+int main(void)
+{
+	printf("%s\n", path("PATH"));
+	return (0);
+}
