5966459e4e9ce13c2bddcb21689aaa32dc30b03a
Mari <916@holbertonschool.com>
2019-09-14 18:52:44 -0500

    function that returns amount of nodes in a double linked list

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * dlistint_len - functiont that returns amount of nodes in a dlinkedlist
+ * @h: header node that points to the list
+ * Return: amount of nodes
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	int nodes = 0;
+
+	while (h)
+	{
+		n++;
+		h = h->next;
+	}
+	return (n);
+}
