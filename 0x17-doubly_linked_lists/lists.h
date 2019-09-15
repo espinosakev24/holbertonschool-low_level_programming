101553fce35d895134a3d2c9ee6f10c38f6373b3
Mari <916@holbertonschool.com>
2019-09-14 19:14:26 -0500

    function that returns amount of nodes in a double linked list

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -1,5 +1,10 @@
 #ifndef HOLBERTON
 #define HOLBERTON
+
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
 /**
  * struct dlistint_s - doubly linked list
  * @n: integer
