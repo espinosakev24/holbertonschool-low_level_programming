9c1d854b77e7f9c9757f34adbe8eb943bc42ca39
Kevin <916@holbertonschool.com>
2019-09-15 22:46:55 -0500

    function that adds a node in a given index

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+/**
+ * insert_dnodeint_at_index - function that adds a node at a given index
+ * @h: dbptr to dblinkedlist
+ * @idx: index
+ * @n: n data member
+ * Return: new_node
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *new_node = malloc(sizeof(dlistint_t));
+	dlistint_t *aux = *h;
+	unsigned int a = 0;
+
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+	new_node->n = n;
+	while (aux != NULL)
+	{
+		if (idx == a + 1)
+		{
+			new_node->prev = aux;
+			aux = aux->next;
+			new_node->next = aux;
+			aux->prev = new_node;
+			aux = new_node->prev;
+			aux->next = new_node;
+		}
+		a++;
+		aux = aux->next;
+	}
+	return (new_node);
+}
