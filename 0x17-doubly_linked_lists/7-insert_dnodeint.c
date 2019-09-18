1c0bd1a0dbb07fb44e11ed7a101abbe3927015f3
Kevin <916@holbertonschool.com>
2019-09-18 20:44:37 +0000

    function that inserts a node at index

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -16,8 +16,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	{
 		return (NULL);
 	}
-	if (!aux)
-		return (NULL);
+	if (*h == NULL && idx == 0)
+		return (add_dnodeint(h, n));
 	while (aux != NULL)
 	{
 		if (idx == a)
