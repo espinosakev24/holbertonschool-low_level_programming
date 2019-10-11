5d4ed9e58e38b58958b14a2f2ed39aa25af4fe96
Mari <916@holbertonschool.com>
2019-10-11 10:55:18 -0500

    fixing header file

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -20,15 +20,14 @@ typedef struct hash_node_s
 /**
  * struct hash_table_s - Hash table data structure
  *
- * @size: The size of the array
  * @array: An array of size @size
  * Each cell of this array is a pointer to the first node of a linked list,
  * because we want our HashTable to use a Chaining collision handling
  */
 typedef struct hash_table_s
 {
-	     unsigned long int size;
-	          hash_node_t **array;
+		unsigned long int size
+		hash_node_t **array;
 } hash_table_t;
 hash_table_t *hash_table_create(unsigned long int size);
 unsigned long int hash_djb2(const unsigned char *str);
