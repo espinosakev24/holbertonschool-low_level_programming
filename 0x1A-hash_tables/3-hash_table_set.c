ac59b969ddf8d338f1b1c209cf6d854768b561e4
Kevin <916@holbertonschool.com>
2019-10-12 01:33:58 -0500

    fixing betty

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -29,7 +29,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	else
 	{
 		aux = ht->array[idx];
-		while(aux)
+		while (aux)
 		{
 			if (strcmp(ht->array[idx]->key, key) == 0)
 			{
