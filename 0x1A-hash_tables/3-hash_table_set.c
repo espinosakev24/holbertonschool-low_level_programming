921395f924123df2ba1fc3af2832ef23b9b0d867
Kevin <916@holbertonschool.com>
2019-10-12 01:32:49 -0500

    Uploading task 3 with the collision implementation

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -8,6 +8,7 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
+	hash_node_t *aux;
 	hash_node_t *hash_node;
 	unsigned long int idx = 0;
 
@@ -16,10 +17,30 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node = malloc(sizeof(hash_node_t));
 	if (hash_node == NULL)
 		return (0);
-
 	idx = key_index((const unsigned char *)key, ht->size);
 	hash_node->key = strdup(key);
 	hash_node->value = strdup(value);
-	ht->array[idx] = hash_node;
-	return (1);
+	hash_node->next = NULL;
+	if (!(ht->array[idx]))
+	{
+		ht->array[idx] = hash_node;
+		return (1);
+	}
+	else
+	{
+		aux = ht->array[idx];
+		while(aux)
+		{
+			if (strcmp(ht->array[idx]->key, key) == 0)
+			{
+				ht->array[idx]->value = strdup(value);
+				return (1);
+			}
+			aux = aux->next;
+		}
+		hash_node->next = ht->array[idx];
+		ht->array[idx] = hash_node->next;
+		return (1);
+	}
+	return (0);
 }
