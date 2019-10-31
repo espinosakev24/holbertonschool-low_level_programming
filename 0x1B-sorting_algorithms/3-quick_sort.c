6ad4a2b0831207613b581f10219b7da4df686bdc
Kevin <916@holbertonschool.com>
2019-10-30 21:46:20 -0500

    uploading quicksort

--- 0x1B-sorting_algorithms/3-quick_sort.c
+++ 0x1B-sorting_algorithms/3-quick_sort.c
@@ -7,20 +7,38 @@
  */
 void quick_sort(int *array, size_t size)
 {
-	size_t n = 0, pos = -1;
+	quick_recursive(array, size, -1, size - 1);
+}
+void quick_recursive(int *array, size_t size, size_t low, size_t high)
+{
+	if (low < high)
+	{
+		piv_pos(array, size, low, high);
+		quick_recursive(array, size, low, high - 1);
+		quick_recursive(array, size, low + 1, high);	
+	}
+}
+size_t piv_pos(int *array, size_t size, size_t low, size_t high)
+{
+	size_t n, pos = low;
 	int temp = 0;
 
-	for (; n < size - 1; n++)
+	for (n = low; n < high; n++)
 	{
-		if (array[n] < array[size - 1])
+		if (array[n] < array[high])
 		{
 			pos++;
 			temp = array[n];
 			array[n] = array[pos];
 			array[pos] = temp;
+			if (n != pos)
+				print_array(array, size);
 		}
 	}
-	temp = array[size - 1];
-	array[size - 1] = array[pos + 1];
-	array[pos + 1] = temp;
+	temp = array[pos];
+	array[pos] = array[high];
+	array[high] = temp;
+	if (pos != high)
+		print_array(array, size);
+	return (pos);
 }
