99c242f650db95962cde8083f1d0123e078d3756
espinosakev24 <916@holbertonschool.com>
2019-12-05 18:33:37 -0500

    uploading task 14: function that calculates the balance of a binary tree

--- 0x1D-binary_trees/14-binary_tree_balance.c
+++ 0x1D-binary_trees/14-binary_tree_balance.c
@@ -7,22 +7,28 @@
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	int left_size = 0, right_size = 0;
-	int end = 0;
+	int left = binary_tree_height(tree->left);
+	int right = binary_tree_height(tree->right);
 
-	end++;
+	return (left - right);
+
+}
+/**
+ * binary_tree_height - function that measures
+ * the height of a binary tree
+ * @tree: pointer to the root node
+ * Return: height of binary tree
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left_size = 1, right_size = 1;
+
 	if (!tree)
 		return (0);
 	if (tree->left)
-		left_size = binary_tree_balance(tree->left) + 1;
+		left_size = binary_tree_height(tree->left) + 1;
 	if (tree->right)
-		right_size = binary_tree_balance(tree->right) + 1;
+		right_size = binary_tree_height(tree->right) + 1;
 
-	end--;
-	if (end == 0)
-		return (left_size - right_size);
-	if (left_size > right_size)
-		return (left_size);
-	else
-		return (right_size);
+	return ((left_size > right_size) ? left_size : right_size);
 }
