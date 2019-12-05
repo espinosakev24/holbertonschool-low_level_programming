ec6d2c269dec325f0be3d0033a49c3648a581b21
espinosakev24 <916@holbertonschool.com>
2019-12-05 17:27:50 -0500

    function that calculates the balance of a binary tree

--- 0x1D-binary_trees/14-binary_tree_balance.c
+++ 0x1D-binary_trees/14-binary_tree_balance.c
@@ -1,5 +1,10 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_balance - function that calc the balance of a binary tree
+ * @tree: pointer to the root to the tree
+ * Return: the balance
+ */
 int binary_tree_balance(const binary_tree_t *tree)
 {
 	size_t left_size = 0, right_size = 0;
