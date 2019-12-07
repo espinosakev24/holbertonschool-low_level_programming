dca01c2c5d1c831da05eca0481915a32e937f064
espinosakev24 <916@holbertonschool.com>
2019-12-06 23:33:07 -0500

    uploading task 103: function that performs a left rotation

--- 0x1D-binary_trees/103-binary_tree_rotate_left.c
+++ 0x1D-binary_trees/103-binary_tree_rotate_left.c
@@ -1,10 +1,30 @@
 #include "binary_trees.h"
-
+/**
+ * binary_tree_rotate_left - function performs a left rotation
+ * @tree: pointer to the root
+ * Return: root node
+ */
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
 {
+	binary_tree_t *r;
+
 	if (!tree)
 		return (NULL);
-	if (!tree->right && !tree->left)
+	if (tree->right)
+	{
+		r = tree->right;
+		if (r->left)
+		{
+			tree->right = r->left;
+			tree->right->parent = tree;
+		}
+		if (!r->left)
+			tree->right = NULL;
+		r->parent = NULL;
+		r->left = tree;
+		tree->parent = r;
+		return (r);
+	}
+	else
 		return (tree);
-	return (tree);
 }
