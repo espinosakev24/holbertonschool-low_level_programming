d140c3ba3650de6027e86fef5ecd30f14037f883
espinosakev24 <916@holbertonschool.com>
2019-12-06 22:53:35 -0500

    function that checks if a binary tree is complete

--- 0x1D-binary_trees/binary_trees.h
+++ 0x1D-binary_trees/binary_trees.h
@@ -57,4 +57,5 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree);
 void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second);
+int binary_tree_is_complete(const binary_tree_t *tree);
 #endif /* HOLBERTONSCHOOL_LOW_LEVEL_PROGRAMMING_BINARY_TREES_H */
