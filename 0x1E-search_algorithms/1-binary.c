5e583458b88b51b778bdeecfb62e4936045bea1b
espinosakev24 <916@holbertonschool.com>
2020-01-15 16:40:12 -0500

    uploades the task 1 -> binary search

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,49 @@
+#include "search_algos.h"
+/**
+ * binary_search - searches bineraly a value in an array
+ * @size: Size of the array
+ * @value: The objective value to search its index
+ * @array: array of integer
+ * Return: the index of the value founded
+ */
+int binary_search(int *array, size_t size, int value)
+{
+	unsigned int low = 0, high = size - 1, mid;
+
+	while (low <= high)
+	{
+		PrintArray(array, low, high);
+		printf("\n");
+		mid = (low + high) / 2;
+		if (array[mid] == value)
+		{
+			return (mid);
+		}
+		else if (array[mid] > value)
+			high = mid - 1;
+		else
+			low = mid + 1;
+	}
+	return (-1);
+}
+/**
+ * PrintArray - print array from index to size
+ * @index: Size of the array
+ * @size: size of the array to print
+ * @array: array of integer
+ * Return: nothing
+ */
+void PrintArray(int *array, int index, int size)
+{
+	printf("Searching in array: ");
+	while (index <= size)
+	{
+		if (index == size)
+			printf("%d", array[index]);
+		else
+		{
+			printf("%d, ", array[index]);
+		}
+		index++;
+	}
+}
