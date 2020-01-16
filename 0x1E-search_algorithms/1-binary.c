455c779ea84b46fda04dbac0fc4f2087c1dfdfea
espinosakev24 <916@holbertonschool.com>
2020-01-15 21:40:41 -0500

    fixes case-> when array is null

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -10,8 +10,8 @@ int binary_search(int *array, size_t size, int value)
 {
 	unsigned int low = 0, high = size - 1, mid;
 
-    if (array == NULL)
-        return (-1);
+	if (array == NULL)
+		return (-1);
 	while (low <= high)
 	{
 		PrintArray(array, low, high);
