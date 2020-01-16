8d40f8bc2b5b88d2c2cee7d504675865a7aa74b4
espinosakev24 <916@holbertonschool.com>
2020-01-15 21:34:53 -0500

    uploades header file

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -2,4 +2,6 @@
 #define HOLBERTONSCHOOL
 #include <stdio.h>
 int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+void PrintArray(int *array, int index, int size);
 #endif
