6b8eb4752452d561437cd372cda291c2b4ef75f4
espinosakev24 <kevinespinosazapata@gmail.com>
2020-07-09 00:06:47 -0500

    adds the std input numbers

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,13 +1,28 @@
 #include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
 /**
  * main - check the code for Holberton School students.
  * @argc: counter of parameters
  * @argv: array of pointers
  * Return: Always 0.
  */
-int main(int argc, __attribute__((unused)) char *argv[])
+int main(int argc, char *argv[])
 {
-	argc = ((argc + 1) * (argc + 2));
-	printf("%d\n", argc);
+	int n = 1, i = 0, result = 0;
+
+	for (n = 1; n < argc; n++)
+	{
+		for (i = 0; argv[n][i] != '\0'; i++)
+		{
+			if (isdigit(argv[n][i]) == 0)
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		result = result + atoi(argv[n]);
+	}
+	printf("%d\n", result);
 	return (0);
 }
